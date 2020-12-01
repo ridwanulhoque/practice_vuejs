710611fe327a04cf2608bf02ba040a462a534893
ridwanhoque <ridwan.abdnews@gmail.com>
2020-12-02 00:31:00 +0600

    event and method

--- 2.php
+++ 2.php
@@ -5,28 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
-    <style>
-        .blue{
-            background-color: blue; width: 200px; height: 200px;
-        }
-
-        .red{
-            background-color: red; width: 200px; height: 200px;
-        }
-    </style>
 </head>
 <body>
     <div id="app">
-        <input type="text" v-on:input="cssClass = $event.target.value">
-        <p>Class {{ cssClass }}</p>
-        <div v-bind:class="cssClass"></div>
+        <!-- <button v-on:click="title = 'New Title'">Change Title</button> -->
+        <p>{{ title }}</p>
+        <button v-on:click="changeTitle">Change Title</button>
     </div>
 
     <script>
         new Vue({
             el: '#app',
             data: {
-                cssClass: ''
+                title: 'Hello World!'
+            },
+            methods: {
+                changeTitle(){
+                    this.title='Changed Title'
+                }
             }
         });
     </script>
